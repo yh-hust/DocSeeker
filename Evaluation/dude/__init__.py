371be0e9f84c852fa289bd4f550dd6dd08a3927d
yh-hust <haoyan@hust.edu.cn>
2026-04-24 17:05:57 +0800

    Update __init__.py

--- Evaluation/dude/__init__.py
+++ Evaluation/dude/__init__.py
@@ -3,25 +3,21 @@ from .eval import calculate_score
 import json
 from tqdm import tqdm
 def append_to_json(file_path, new_data):
-    # 读取已有数据（如果存在），并追加新数据
-    # 将合并后的数据保存回文件
     with open(file_path, 'w', encoding='utf-8') as f:
         json.dump(new_data, f, ensure_ascii=False, indent=4)
 
 def score_cal_and_save(data_list, save_path):
-    #import pdb;pdb.set_trace()
+
     gt_answers = [item['answer'] for item in data_list]
     preds = [item['extracted_res'] for item in data_list]
     answer_types = [item["evidence_sources"] for item in data_list]
     scores = []
     
-    #import pdb;pdb.set_trace()
     if not all(x == '' for x in gt_answers):
         assert len(preds) == 6318
         for gt,pred,answer_type in tqdm(zip(gt_answers,preds,answer_types)):
             scores.append(calculate_score([pred],gt,answer_type))
-        
-        #import pdb;pdb.set_trace()
+
         for item,score in zip(data_list,scores):
             item['anls_score'] = score
         
@@ -33,16 +29,14 @@ def score_cal_and_save(data_list, save_path):
         with open(txt_path, "w", encoding="utf-8") as f:
             f.write(f"Average Anls: {avg_anls:.4f}\n")
     else:
-        #import pdb;pdb.set_trace()
         assert len(preds) == 11402
-        #import pdb;pdb.set_trace()
         ori_data_list = json.load(open('dude/sample_test.json'))
         submit_data_list = []
         save_path = save_path.replace(".json","_submit.json")
         for pred,ori in zip(preds,ori_data_list):
             sample = {
                 "questionId":ori["questionId"],
-                "answer":pred,
+                "answer":pred if pred.lower() != "not answerable" else "",
                 "answer_confidence":1.0,
                 "answer_abstain":False
             }
